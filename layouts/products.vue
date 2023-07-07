cf1641d528ec79ba3ccfe85a7cb34933d3716e3a
bakr <alqassab31@gmail.com>
2023-07-07 11:44:02 +0300

    Add tailwind and layouts

---
+++ layouts/products.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <header>
+                  Header
+        </header>
+        <div>
+            <slot />
+        </div>
+        <footer>
+            Footer
+
+            <nav>
+                <nuxt-link to="/"> Nuxt Project</nuxt-link>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="/about">About</nuxt-link></li>
+                    <i><nuxt-link to="products">Products</nuxt-link></i>
+                </ul>
+            </nav>
+        </footer>
+    </div>
+</template>
+<style scoped>
+
+.router-link-exact-active {
+    color: #12b488;
+}
+footer {
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+    width: 95%;
+    overflow: hidden;
+}
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -7,6 +7,10 @@
   
 <script setup>
 
+definePageMeta({
+    layout:'products'
+})
+
 </script>
   
 <style scoped></style>
